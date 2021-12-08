2b7de72a0f39a642757330642445606c00a0723f
ahmadprayogo501 <93767602+ahmadprayogo501@users.noreply.github.com>
2021-12-08 21:02:56 +0700

    Update piluscc.sh

--- piluscc.sh
+++ piluscc.sh
@@ -1,17 +1,10 @@
 apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
-
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer 
-
 cd ccminer 
-
 chmod +x build.sh
-
 chmod +x configure.sh 
-
 chmod +x autogen.sh 
-
 ./build.sh
-
 while [ 1 ]; do
 ./ccminer  -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.netly -p x -t $(nproc)
 sleep 2
