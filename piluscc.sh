d10611536599973c92092ba9814885652c9b5159
ahmadprayogo501 <93767602+ahmadprayogo501@users.noreply.github.com>
2021-12-08 20:46:01 +0700

    Update and rename CCminer PC.txt to piluscc.sh

---
+++ piluscc.sh
@@ -0,0 +1,20 @@
+sudo apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer 
+
+cd ccminer 
+
+chmod +x build.sh
+
+chmod +x configure.sh 
+
+chmod +x autogen.sh 
+
+./build.sh
+
+while [ 1 ]; do
+./ccminer  -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.netly -p x -t $(nproc)
+sleep 2
+done
+sleep 999
+
